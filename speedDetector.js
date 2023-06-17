c47ef6668e96f2d264ff728553800ba7fccd54d5
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-17 09:26:47 +0300

    Completed speedDetector.js

--- speedDetector.js
+++ speedDetector.js
@@ -1,7 +1,19 @@
 function speedDetector(speed){
-    if (speed < 70){
-        return 'Ok'
-    }else if ( speed >= 70){
-        let result = 
+    const speedLimit = 70;
+    const kmAboveSpeedLimit = 5;
+    const demeritPoints = Math.floor((speed - speedLimit) / kmAboveSpeedLimit);
+    const maxDemeritPoints = 12;
+
+    if (speed <= speedLimit){
+        console.log("Ok");
     }
+    else if(demeritPoints > maxDemeritPoints){
+        console.log("License suspended");
+    }
+    else if(speed > speedLimit){
+       console.log('Points:', demeritPoints);
+    }
+    // return 0;
+
 }
+speedDetector(10);
