fffbf867a1eb42391aa756f913e9362d9a9909e5
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-18 20:18:23 +0300

    Complete assignment

---
+++ README.md
@@ -0,0 +1,9 @@
+This directory cotains three JavaScrpt files;
+    1. The netSalaryCalculator file:
+    This file takes basic salary and benefits and calculates the Net salary. 
+
+    2. The speedDetector file:
+    This file contains a speed detector and a feature to give demerit points to speeds above the speed limit.
+
+    3. The studentGradeGenerator file:
+    This contains a student grader that alloates specific grades for certain marks.
---
+++ netSalaryCalculator.js
@@ -0,0 +1,104 @@
+const grossSalary = basicSalary + benefits;
+//const taxableIncome = basicSalary + benefits - totalDeductions;
+const totalDeductions = resultNHIF /* NSSFDeductions()*/;
+//const payee = result;
+//const taxableIncome = result - totalDeductions;
+const netSalary = grossSalary - payee - totalDeductions;
+
+function calcutateNetSalary(basicSalary, benefits){
+   function NHIFDeductions(grossSalary){
+    if(grossSalary <= 5999){
+        let resultNHIF = grossSalary - 150;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 6000 && grossSalary <= 7999){
+        let resultNHIF = grossSalary - 300;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 8000 && grossSalary <= 11999){
+        let resultNHIF = grossSalary - 400;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 12000 && grossSalary <= 14999){
+        let resultNHIF = grossSalary - 500
+    }
+    else if(grossSalary>= 15000 && grossSalary <= 19999){
+        let resultNHIF = grossSalary - 600;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 20000 && grossSalary <= 24999){
+        let resultNHIF = grossSalary - 750;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 25000 && grossSalary <= 29999){
+        let resultNHIF = grossSalary - 850;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 30000 && grossSalary <= 34999){
+        let resultNHIF = grossSalary - 900;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 35000 && grossSalary <= 39999){
+        let resultNHIF = grossSalary - 950;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 40000 && grossSalary <= 44900){
+        let resultNHIF = grossSalary - 1000;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 45000 && grossSalary <= 49999){
+        let resultNHIF = grossSalary - 1100;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 50000 && grossSalary <= 59999){
+        let resultNHIF = grossSalary - 1200;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 60000 && grossSalary <= 69999){
+        let resultNHIF = grossSalary - 1300;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 70000 && grossSalary <= 79999){
+        let resultNHIF = grossSalary - 1400;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 80000 && grossSalary <= 89999){
+        let resultNHIF = grossSalary - 1500;
+        return resultNHIF;
+    }
+    else if(grossSalary>= 90000 && grossSalary <= 99999){
+        let resultNHIF = grossSalary - 1600;
+        return resultNHIF;
+    }
+    else if(grossSalary >= 100000){
+        let resultNHIF = grossSalary - 1700;
+        return resultNHIF;
+    }
+}
+
+
+function payeeCalculator(grossSalary){
+    //const taxableIncome = basicSalary + benefits - totalDeductions;
+    if(grossSalary <= 24000){
+        let result = grossSalary * 0.1;
+        return result;
+    }else if (grossSalary >= 24001 && grossSalary <= 32333){
+        let result = grossSalary * 0.25;
+        return result;
+    }else if (grossSalary > 32333){
+        let result = grossSalary * 0.3;
+        return result;
+    }  
+}
+}
+
+
+grossSalaryCalculator();
+basicSalary(12000)
+benefits(500)
+console.log(payee);
+
+/*function NSSFDeductions(grossSalary){
+    if()
+}
+*/
